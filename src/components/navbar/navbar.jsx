651f7a9eb32c8662635d1fe06b4151e27537830d
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2024-11-23 11:51:28 -0300

    commit inicial

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,69 @@
+import "./navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/logo-white.png";
+import api from "../../constants/api";
+
+function Navbar() {
+
+    const navigate = useNavigate();
+
+    function Logout() {
+        localStorage.removeItem("sessionToken");
+        localStorage.removeItem("sessionId");
+        localStorage.removeItem("sessionEmail");
+        localStorage.removeItem("sessionName");
+        api.defaults.headers.common['authorization'] = "";
+        navigate("/");
+    }
+
+    return <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-primary" data-bs-theme="dark">
+        <Link className="navbar-brand" to="/appointments">
+            <img className="navbar-logo" src={logo} />
+        </Link>
+        <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link className="nav-link active" to="/appointments">Agendamentos</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/barbers">Barbeiros</Link>
+                    </li>
+                </ul>
+                <ul className="navbar-nav ">
+                    <li className="nav-item">
+                        <div className="btn-group">
+                            <button type="button" className="btn btn-primary dropdown-toggle px-2" data-bs-toggle="dropdown" aria-expanded="false">
+                                {localStorage.getItem("sessionName")}
+                            </button>
+                            <ul className="dropdown-menu dropdown-menu-end">
+                                <li><Link className="dropdown-item" to="#">Meu Perfil</Link></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><button className="dropdown-item" onClick={Logout}>Desconectar</button></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+}
+
+export default Navbar;
+
+{/* <nav className="navbar  navbar-expand-lg bg-primary">
+
+
+
+      
+
+        </ul>
+    </div>
+
+</div>
+
+</nav> */}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+// import Login from './pages/login/login'
+import "./styles/global.css"
+import Rotas from "./rotas"
+
+createRoot(document.getElementById('root')).render(
+  
+  <Rotas/>
+  
+)
