f1d79cc87a599ed41ed5e7c798a5172b1e578989
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-02-02 16:54:19 -0300

    modal

--- src/pages/appointments/appointments.jsx
+++ src/pages/appointments/appointments.jsx
@@ -38,41 +38,6 @@ function Appointments() {
         ]
     });
     }
-    // return (
-    //     <>
-    //         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    //             <div class="modal-dialog">
-    //                 <div class="modal-content">
-    //                     <div class="modal-header">
-    //                         <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-    //                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-    //                     </div>
-    //                     <div class="modal-body">
-    //                         ...
-    //                     </div>
-    //                     <div class="modal-footer">
-    //                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-    //                         <button type="button" class="btn btn-primary">Save changes</button>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </>
-    // )
-    // confirmAlert({
-    //     title: "Exclusão",
-    //     message: "Confirma exclusão desse agendamento?",
-    //     buttons: [
-    //         {
-    //             label: "Sim",
-    //             onClick: () => DeleteAppointment(id_appointment)
-    //         },
-    //         {
-    //             label: "Não",
-    //             onClick: () => { }
-    //         }
-    //     ]
-    // });
 
     async function DeleteAppointment(id) {
         try {
@@ -90,7 +55,7 @@ function Appointments() {
                 alert(error.response?.data.error);
             }
             else
-                alert("Erro ao excluir dados");
+                alert("Erro ao excluir reserva");
         }
     }
 
