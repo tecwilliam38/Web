c864c4fe1448a8ccabdbc344a14dee50966e4538
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-07-07 19:00:39 -0300

    api update

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const VERCELAPI = "https://powertech-api.vercel.app/";
-// https://powertech-api.vercel.app/users/register
+const VERCELAPI = "https://power-tech-api.vercel.app/";
+// const VERCELAPI = "https://powertech-api.vercel.app/";
 const LOCALHOST = "http://192.168.1.65:3000";
 
 const api = axios.create({
