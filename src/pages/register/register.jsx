43b8c7967207e88751c08eea2ab30952736faf67
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-07-14 16:27:37 -0300

    técnico

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -12,7 +12,7 @@ function RegisterTecnicoComponent() {
     const [name, setName] = useState()
     const [cel_phone, setCel_phone] = useState();
     const [endereco, setEndereco] = useState();
-    const [email, setEmail] = useState()
+    const [email, setEmail] = useState("")
     const [specialty, setSpecialty] = useState("");
     const [password, setPassword] = useState("");
     const [password2, setPassword2] = useState("");
@@ -58,94 +58,79 @@ function RegisterTecnicoComponent() {
     }
 
 
-    return <div className="bgImage">
-        <Navbar/>
-        <div className="col-sm-5 d-flex justify-content-center align-items-center text-center">
-
-            <form className="form-signin">
-                {/* <img src={logo} className="logo w-100 mb-4" /> */}
-                <h5 className="mb-5">Crie sua conta agora mesmo.</h5>
-                <h5 className="mb-4 text-secondary">Preencha os campos abaixo</h5>
-
-                <div className="mt-4">
-                    <input type="text" placeholder="Nome"
-                        className="form-control"
-                        onChange={(e) => setName(e.target.value)} />
-                </div>
-                <div className="mt-2">
-                    <input type="tel" placeholder="Celular"
-                        className="form-control"
-                        onChange={(e) => setCel_phone(e.target.value)} />
-                </div>
-                <div className="mt-2">
-                    <input type="text" placeholder="Endereço"
-                        className="form-control"
-                        onChange={(e) => setEndereco(e.target.value)} />
-                </div>
-                <div className="mt-2">
-                    <input type="text" placeholder="Competência"
-                        className="form-control"
-                        onChange={(e) => setSpecialty(e.target.value)} />
-                </div>
-                <div className="mt-2">
-                    <input type="email" placeholder="E-mail"
-                        className="form-control"
-                        onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className="mt-2 position-relative">
-                    <input type={visible ? "text" : "password"} placeholder="Senha"
-                        className="form-control"
-                        onChange={(e) => setPassword(e.target.value)} />
-                    <i
-                        className={`bi ${visible ? "bi-eye-slash" : "bi-eye"} position-absolute`}
-                        onClick={() => setVisible(!visible)}
-                        style={{
-                            fontSize: "1.3rem",
-                            top: "50%",
-                            right: "15px",
-                            transform: "translateY(-50%)",
-                            cursor: "pointer",
-                            color: "#7b7e80"
-                        }}
-                    ></i>
-                </div>
-                <div className="mt-2 positio-relative">
-                    <input  type={visible ? "text" : "password"} placeholder="Confirme a senha"
-                        className="form-control"
-                        onChange={(e) => setPassword2(e.target.value)} />
-                    <i
-                        className={`bi ${visible ? "bi-eye-slash" : "bi-eye"} position-absolute`}
-                        onClick={() => setVisible(!visible)}
-                        style={{
-                            fontSize: "1.3rem",
-                            top: "50%",
-                            right: "15px",
-                            transform: "translateY(-50%)",
-                            cursor: "pointer",
-                            color: "#7b7e80"
-                        }}
-                    ></i>
-                </div>
-                <div className="mt-3 mb-5">
-                    <button onClick={ExecuteAccount} className="btn btn-primary w-100" type="button">
-                        Criar minha conta
-                    </button>
-                </div>
-
-                {
-                    msg.length > 0 &&
-                    <div className="alert alert-danger" role="alert">
-                        {msg}
+    return <>
+        <Navbar />
+        <div className="container-fluid justify-content-center align-items-center mt-page">
+            <div className="container mt-5 border p-3 rounded shadow">
+                <h3>📄 Ficha de Cadastro de Técnicos</h3>
+                <form className="mt-4">
+                    <div className="mb-3">
+                        <label className="form-label">Nome do Técnico</label>
+                        <input
+                            type="text"
+                            name="escola"
+                            className="form-control"
+                            onChange={(e) => setName(e.target.value)}
+                        />
                     </div>
-                }
 
-                <div>
-                    <span className="me-1">Já tenho uma conta.</span>
-                    <Link to="/" className="ml-2">Acessar agora!</Link>
-                </div>
-            </form>
+                    <div className="mb-3">
+                        <label className="form-label">Celular</label>
+                        <input
+                            type="tel"
+                            name="Celular"
+                            placeholder="Celular"
+                            className="form-control"
+                            onChange={(e) => setCel_phone(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Endereço</label>
+                        <input type="text" placeholder="Endereço"
+                            className="form-control"
+                            onChange={(e) => setEndereco(e.target.value)} />
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label">Skill</label>
+                        <input type="text" placeholder="Skill"
+                            className="form-control"
+                            onChange={(e) => setSpecialty(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">E-mail</label>
+                        <input type="email" placeholder="E-mail"
+                            value={email}
+                            className="form-control"
+                            onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="mt-2 position-relative">
+                        <input type={visible ? "text" : "password"} placeholder="Senha"
+                            className="form-control"
+                            onChange={(e) => setPassword(e.target.value)} />
+                        <i
+                            className={`bi ${visible ? "bi-eye-slash" : "bi-eye"} position-absolute`}
+                            onClick={() => setVisible(!visible)}
+                            style={{
+                                fontSize: "1.3rem",
+                                top: "50%",
+                                right: "15px",
+                                transform: "translateY(-50%)",
+                                cursor: "pointer",
+                                color: "#7b7e80"
+                            }}
+                        ></i>
+                    </div>
+
+                    <div className="mt-3 mb-5">
+                        <button onClick={ExecuteAccount} className="btn btn-primary w-100" type="button">
+                            Cadastrar técnico
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
+    </>
 }
 
 export default RegisterTecnicoComponent;
